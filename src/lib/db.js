20b057e2fcd6c0e13067404793e43e1e0de32a5d
Soha Chauhan <sohachauhan2@gmail.com>
2025-06-26 14:48:54 +0530

    error resolved

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,20 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) throw new Error("MONGODB_URI not defined in .env.local");
-
-let cached = global.mongoose || { conn: null, promise: null };
-
 export async function connectToDatabase() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+  try {
+    const client = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err.message);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
