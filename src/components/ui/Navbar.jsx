57e976e44f93715f4cb4ea85f09bf48fea15c55d
Soha Chauhan <sohachauhan2@gmail.com>
2025-07-24 01:22:08 +0530

    `Update Navbar component: replace img with Image component, add width and height props, and update hr element`

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -48,8 +48,10 @@ const Navbar = ({ user }) => {
       <div className="flex items-center lg:space-x-6 space-x-4">
         <div className="flex items-center flex-shrink-0">
           <Link href="/dashboard">
-            <img
-              src="/Logo-Full.png"
+            <Image
+              width="100"
+              height="100"
+              src="/logo-full.png"
               alt="Spenza Logo"
               className="h-10 w-auto"
             />
@@ -89,12 +91,12 @@ const Navbar = ({ user }) => {
         <div className="absolute top-full left-0 w-full bg-white shadow-lg border-t flex flex-col items-center md:hidden animate-fade-up z-50">
           <div className="flex flex-col w-full font-semibold text-gray-700 px-6 py-4  ">
             {navLinks}
-            <hr className="w-full border-gray-200 mt-3 mb-1"/>
+            <hr className="w-full border-gray-200 mt-3 mb-1" />
             <div className="flex items-center text-sm text-gray-600 mt-4 mb-4 ">
               <CircleUserRound className="w-5 h-5 mr-1" />
               {user.name}
             </div>
-           
+
             <button
               onClick={() => {
                 setMenuOpen(false);
